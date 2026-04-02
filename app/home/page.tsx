81e072a3c57cfed2af6b622e6857fc70bd41eb6d
Jonas Larsson <jl.7804@gmail.com>
2026-04-02 18:26:25 +0200

    strict

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+'use client'
 
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
@@ -35,4 +35,3 @@ export default function HomePage() {
     </main>
   )
 }
-
