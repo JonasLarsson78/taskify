7290c03a685590a09eafa96d4015f5ec435c51f3
Jonas Larsson <jl.7804@gmail.com>
2026-04-05 19:23:02 +0200

    new mobile imge

--- app/head.tsx
+++ app/head.tsx
@@ -2,7 +2,21 @@ export default function Head() {
   return (
     <>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* Prevent theme flash: set data-theme on html before hydration */}
+      {/* Favicons */}
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="icon" href="/favicon.ico" />
+      {/* Android Chrome icons */}
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-chrome-192x192.png" />
+      <link rel="icon" type="image/png" sizes="512x512" href="/android-chrome-512x512.png" />
+      {/* Apple Touch Icon */}
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      {/* SVGs for reference (not standard for favicons, but can be used elsewhere) */}
+      {/* <link rel="icon" type="image/svg+xml" href="/file.svg" /> */}
+      {/* <link rel="icon" type="image/svg+xml" href="/globe.svg" /> */}
+      {/* <link rel="icon" type="image/svg+xml" href="/next.svg" /> */}
+      {/* <link rel="icon" type="image/svg+xml" href="/vercel.svg" /> */}
+      {/* <link rel="icon" type="image/svg+xml" href="/window.svg" /> */}
       <script
         dangerouslySetInnerHTML={{
           __html: `
@@ -22,7 +36,7 @@ export default function Head() {
           `,
         }}
       />
-      <link rel="manifest" href="/manifest.json" />
+      <link rel="manifest" href="/site.webmanifest" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-title" content="Taskify" />
       <meta name="mobile-web-app-capable" content="yes" />
@@ -30,7 +44,6 @@ export default function Head() {
         name="apple-mobile-web-app-status-bar-style"
         content="black-translucent"
       />
-      <link rel="apple-touch-icon" href="/icon-192.png" />
       <meta name="theme-color" content="#18192a" />
     </>
   )
