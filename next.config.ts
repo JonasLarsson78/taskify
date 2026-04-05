831ab2c1d62717d283c9211a7f0ad68c233549e0
Jonas Larsson <jl.7804@gmail.com>
2026-04-05 11:42:18 +0200

    dev error and loader text

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+}
 
-export default nextConfig;
+export default nextConfig
+
+/** @type {import('next').NextConfig} */
+/* const nextConfig = {
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.cache = false
+    }
+    return config
+  },
+}
+
+module.exports = nextConfig */
